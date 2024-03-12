8d91fe76413c74a5af3aa35976fc4a59e0f37abb
Simon Bihel <sbihel@users.noreply.github.com>
2024-03-12 13:00:54 +0000

    Revert addition of PEM conversion functions (#13)

--- Sources/WalletSdk/CryptoUtils.swift
+++
@@ -1,8 +0,0 @@
-import SpruceIDWalletSdkRs
-
-public func pkcs8ToSec1(pem: String) throws -> String {
-    return try SpruceIDWalletSdkRs.pkcs8ToSec1(pem: pem)
-}
-public func sec1ToPkcs8(pem: String) throws -> String {
-    return try SpruceIDWalletSdkRs.sec1ToPkcs8(pem: pem)
-}
