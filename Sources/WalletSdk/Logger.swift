b33ede3d717613fbe6a2134f6aa9e539e526cb96
Todd Showalter <todd.showalter@spruceid.com>
2024-06-12 12:19:28 -0400

    Comment style changed.

--- Sources/WalletSdk/Logger.swift
+++ Sources/WalletSdk/Logger.swift
@@ -1,19 +1,10 @@
-// File: Logger
-//
-//    Copy log messages to the system logger.
-
-//
-// Imports
-//
+/// Logger
+///
+/// Copy log messages to the system logger.
 
 import Foundation
 
-//
-// Code
-//
-
-// Class: SpruceKitLogger
-//    A logging system for debug purposes.
+/// A logging system for debug purposes.
 
 class SpruceKitLogger: NSObject {
    // Make this a singleton.
@@ -31,18 +22,17 @@ class SpruceKitLogger: NSObject {
 
    var verbosity = Verbosity.trace
 
-   // Local Method: logRaw()
-   //    The core logging interface; all log statements pass through this, so
-   // the formatting, decision making &c. are centrallized.  It also means we
-   // can mutex if necessary; if we're feeding the system logger, for example,
-   // we may need to break a single log string into multiple lines, at which
-   // point we'll want some defenses in place against conncurrent logs
-   // interleaving.
-   //
-   // Arguments:
-   //    verb   - verbosity for the log
-   //    prefix - string to prefix on the log line
-   //    text   - the text to be logged
+   /// The core logging interface; all log statements pass through this, so
+   /// the formatting, decision making &c. are centrallized.  It also means we
+   /// can mutex if necessary; if we're feeding the system logger, for example,
+   /// we may need to break a single log string into multiple lines, at which
+   /// point we'll want some defenses in place against conncurrent logs
+   /// interleaving.
+   ///
+   /// - Parameters:
+   ///    - verb:    verbosity for the log
+   ///    - prefix:  string to prefix on the log line
+   ///    - text:    the text to be logged
 
    private func logRaw(verb:   Verbosity,
                        prefix: String,
@@ -52,47 +42,41 @@ class SpruceKitLogger: NSObject {
       print("[\(prefix)] \(text)")
    }
 
-   // Method: logTrace()
-   //    Log a string at "trace" verbosity.
-   //
-   // Arguments:
-   //    text - the text to log
+   /// Log a string at "trace" verbosity.
+   ///
+   /// - Parameters:
+   ///    - text: the text to log
 
    func logTrace(text: String) {
       logRaw(verb: Verbosity.trace, prefix: "Tr", text: text)
    }
 
-   // Method: logInfo()
-   //    Log a string at "info" verbosity.
-   //
-   // Arguments:
-   //    text - the text to log
+   /// Log a string at "info" verbosity.
+   ///
+   /// - Parameters:
+   ///    - text: the text to log
 
    func logInfo(text: String) {
       logRaw(verb: Verbosity.info, prefix: "In", text: text)
    }
 
-   // Method: logWarn()
-   //    Log a string at "warn" verbosity.
-   //
-   // Arguments:
-   //    text - the text to log
+   /// Log a string at "warn" verbosity.
+   ///
+   /// - Parameters:
+   ///    - text: the text to log
 
    func logWarn(text: String) {
       logRaw(verb: Verbosity.warn, prefix: "W?", text: text)
    }
 
-   // Method: logError()
-   //    Log a string at "error" verbosity.
-   //
-   // Arguments:
-   //    text - the text to log
+   /// Log a string at "error" verbosity.
+   ///
+   /// - Parameters:
+   ///    - text: the text to log
 
    func logError(text: String) {
       logRaw(verb: Verbosity.error, prefix: "E!", text: text)
    }
 }
 
-//
 // Copyright © 2024, Spruce Systems, Inc.
-//
